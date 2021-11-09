c89ae5ccc2d7742dd72e3ea32b925f45fa05f795
hailinkim <haikim20@amherst.edu>
2021-11-09 14:25:24 -0500

    Converted back to wide data set after dealing with missing values

--- wrangling_angelica.R
+++ wrangling_angelica.R
@@ -4,25 +4,30 @@ library(dplyr)
 #rating data
 rating <- read_csv("/Users/angelica/Desktop/2019 Part C and D Medicare Star Ratings Data (v04 12 2019) [ZIP, 9MB]/2019 Star Ratings Fall Release (11_2018)/stars.csv")
 write_csv(rating, "rating.csv")
-master <- read_csv("rating.csv")
-rating2 <- as.data.frame(rating[, c(1:5, 28:39)])
+master <- read_csv("data/rating.csv")
+rating <- as.data.frame(master[, c(1:5, 28:39)])
 
 #make the first row to be column names
-names(rating2) <- rating2[1,]
-rating2 <- rating2[-1,]
+names(rating) <- rating[1,]
+rating <- rating[-1,]
 #make the next row(rating variables) to be column names
-colnames(rating2)[-c(1:5)] <- rating2[1, -c(1:5)]
+colnames(rating)[-c(1:5)] <- rating[1, -c(1:5)]
 #remove the time frame row
-rating2 <- rating2[-c(1,2),]
+rating <- rating[-c(1,2),]
 #rename the broken column
-colnames(rating2)[17] <- "C34: Call Center_Foreign Language Interpreter and TTY Availability"
+colnames(rating)[17] <- "C34: Call Center_Foreign Language Interpreter and TTY Availability"
+
+
 #convert to long dataset to remove missing values(e.g. plan too small to be measured)
-rating_long <- rating2 %>% 
+rating_long <- rating %>% 
   pivot_longer(cols = "C23: Getting Needed Care":"C34: Call Center_Foreign Language Interpreter and TTY Availability",
                names_to = "measure",
                names_prefix = "C\\d+: ",
                values_to = "ratings") %>% 
   filter(ratings %in% c(1:5))
+rating_wide <- rating_long %>% 
+  pivot_wider(names_from = "measure", values_from = "ratings") %>% 
+  drop_na()
 
 ############################
 #survey data
