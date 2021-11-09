49fec5c5ab3f96144e0a84e1744b490415346114
hailinkim <haikim20@amherst.edu>
2021-11-09 11:56:00 -0500

    changed variable names

--- wrangling_angelica.R
+++ wrangling_angelica.R
@@ -4,30 +4,23 @@ library(dplyr)
 rating <- read_csv("/Users/angelica/Desktop/2019 Part C and D Medicare Star Ratings Data (v04 12 2019) [ZIP, 9MB]/2019 Star Ratings Fall Release (11_2018)/stars.csv")
 write_csv(rating, "rating.csv")
 master <- read_csv("rating.csv")
-rating <- as.data.frame(master[, c(1:5, 28:39)])
+rating2 <- as.data.frame(rating[, c(1:5, 28:39)])
 
 #make the first row to be column names
-names(rating) <- rating[1,]
-rating <- rating[-1,]
+names(rating2) <- rating2[1,]
+rating2 <- rating2[-1,]
 #make the next row(rating variables) to be column names
-colnames(rating)[-c(1:5)] <- rating[1, -c(1:5)]
+colnames(rating2)[-c(1:5)] <- rating2[1, -c(1:5)]
 #remove the time frame row
-rating <- rating[-c(1,2),]
+rating2 <- rating2[-c(1,2),]
 #rename the broken column
-colnames(rating)[17] <- "C34: Call Center_Foreign Language Interpreter and TTY Availability"
+colnames(rating2)[17] <- "C34: Call Center_Foreign Language Interpreter and TTY Availability"
 #convert to long dataset to remove missing values(e.g. plan too small to be measured)
-rating_long <- rating %>% 
+rating_long <- rating2 %>% 
   pivot_longer(cols = "C23: Getting Needed Care":"C34: Call Center_Foreign Language Interpreter and TTY Availability",
                names_to = "measure",
                names_prefix = "C\\d+: ",
                values_to = "ratings") %>% 
   filter(ratings %in% c(1:5))
 
-
-
-
-coverage <- read_csv("countylevel_2019.csv")
-autauga2 <- coverage %>% 
-  filter(county_name == "Autauga County") %>% 
-  count(agecat, racecat, sexcat, iprcat)
 
