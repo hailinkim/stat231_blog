574cd3d02f214b359a4277e8410012a1b427fb30
hailinkim <haikim20@amherst.edu>
2021-11-09 10:09:08 -0500

    Converted from wide to long

--- wrangling_angelica.R
+++ wrangling_angelica.R
@@ -1,5 +1,6 @@
 library(readr)
 library(tidyverse)
+library(dplyr)
 rating <- read_csv("/Users/angelica/Desktop/2019 Part C and D Medicare Star Ratings Data (v04 12 2019) [ZIP, 9MB]/2019 Star Ratings Fall Release (11_2018)/stars.csv")
 write_csv(rating, "rating.csv")
 master <- read_csv("rating.csv")
@@ -12,10 +13,19 @@ rating <- rating[-1,]
 colnames(rating)[-c(1:5)] <- rating[1, -c(1:5)]
 #remove the time frame row
 rating <- rating[-c(1,2),]
+#rename the broken column
+colnames(rating)[17] <- "C34: Call Center_Foreign Language Interpreter and TTY Availability"
+#convert to long dataset to remove missing values(e.g. plan too small to be measured)
+rating_long <- rating %>% 
+  pivot_longer(cols = "C23: Getting Needed Care":"C34: Call Center_Foreign Language Interpreter and TTY Availability",
+               names_to = "measure",
+               names_prefix = "C\\d+: ",
+               values_to = "ratings") %>% 
+  filter(ratings %in% c(1:5))
 
-tmp <- as.data.frame(rating[1, c(6:11)])
-names(tmp) <- tmp[1,]
-rating <- rating[-1,]
+
+
+
 coverage <- read_csv("countylevel_2019.csv")
 autauga2 <- coverage %>% 
   filter(county_name == "Autauga County") %>% 
