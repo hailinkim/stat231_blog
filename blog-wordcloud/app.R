ee569b02fe4f174049f273ace4fc0f9e244d8bd6
hailinkim <haikim20@amherst.edu>
2021-12-06 12:23:26 -0500

    Tweaked legend

--- blog-wordcloud/app.R
+++ blog-wordcloud/app.R
@@ -2,6 +2,7 @@ library(readr)
 library(shiny)
 library(dplyr)
 library(wordcloud)
+library(extrafont)
 
 ratings <- read_csv("ratings.csv", show_col_types = FALSE)
 # for selectInput
@@ -22,7 +23,7 @@ shinyApp(
         # Input: Animation with custom interval (in ms) ----
         # to control speed, plus looping
         sliderInput("animation", "Year:",
-                    min = 2016, max = 2022,
+                    min = 2016, max = 2021,
                     value = 2016, step = 1,
                     animate = animationOptions(interval = 500, loop = TRUE)),
         
@@ -36,7 +37,7 @@ shinyApp(
       
       # Main panel for displaying outputs ----
       mainPanel(
-        plotOutput("wordcloud", width = "100%", height = 650)
+        plotOutput("wordcloud", width = "100%", height = "415px")
       )
     )
   ),
@@ -52,12 +53,15 @@ shinyApp(
       
       set.seed(1120)
       dat %>%
-        with(wordcloud(words = sentences, freq = mean, scale = c(2,0.8), 
-                       colors = my_pal[as.factor(rating_type)], ordered.colors = TRUE))
+        with(wordcloud(words = sentences, freq = mean, scale = c(1.2, 0.57), 
+                       colors = my_pal[as.factor(rating_type)], ordered.colors = TRUE),
+             family = "Lato Hairline")
       #need to adjust position
-      legend(0, 1, legend = levels(factor(ratings$rating_type)),
-             text.col=brewer.pal(3, "Dark2")[unique(factor(ratings$rating_type))])
-    })
+      legend(0.8, 0.8, title = "Type of Metrics",
+             legend = levels(factor(ratings$rating_type)),
+             fill = brewer.pal(3, "Dark2")[unique(factor(ratings$rating_type))], cex = 0.6)
+    }
+    )
   },
   options = list(height = 600)
 )
